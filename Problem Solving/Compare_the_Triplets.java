c55369c0f85954e0127c28a0010bbcf7415bece6
Sadiqun Nur Ayan <65063367+ayan978@users.noreply.github.com>
2020-10-26 12:06:33 +0600

    Update Compare_the_Triplets.java

--- Problem Solving/Compare_the_Triplets.java
+++ Problem Solving/Compare_the_Triplets.java
@@ -1,13 +1,7 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
-public class Compare_the_Triplets {
 
+  public class Compare_the_Triplets{
     // Complete the compareTriplets function below.
     static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
                 int Alice=0,Bob=0;
@@ -24,45 +18,24 @@ public class Compare_the_Triplets {
                 c.add(Bob);
 
                 return c;
-
-
+                
     }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String[] aItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        List<Integer> a = new ArrayList<>();
-
-        for (int i = 0; i < 3; i++) {
-            int aItem = Integer.parseInt(aItems[i]);
-            a.add(aItem);
-        }
-
-        String[] bItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        List<Integer> b = new ArrayList<>();
-
-        for (int i = 0; i < 3; i++) {
-            int bItem = Integer.parseInt(bItems[i]);
-            b.add(bItem);
-        }
-
-        List<Integer> result = compareTriplets(a, b);
-
-        for (int i = 0; i < result.size(); i++) {
-            bufferedWriter.write(String.valueOf(result.get(i)));
-
-            if (i != result.size() - 1) {
-                bufferedWriter.write(" ");
+    
+    public static void main(String[] args){
+            Scanner s = new Scanner(System.in);
+            List<Integer> a = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+                int aItem = s.nextInt();
+                a.add(aItem);
+            }   
+            List<Integer> b = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+                int bItem = s.nextInt();
+                b.add(bItem);
+            }  
+            List<Integer> result = compareTriplets(a, b);
+            for(int i=0;i<result.size();i++){
+            System.out.print(result.get(i)+" ");
             }
-        }
-
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
     }
-}
+   }
